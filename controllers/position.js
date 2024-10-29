443207b9217c85555481385ecd2d03f9a866f803
Dmitry Karuev <dmkaruev@mail.ru>
2024-10-29 15:48:51 +0300

    29.10.2024 16:00 : Install Libs, Models, Routes

---
+++ controllers/position.js
@@ -0,0 +1,11 @@
+// module.exports.remove = function (req, res) {
+    
+// }
+
+// module.exports.create = function (req, res) {
+    
+// }
+
+// module.exports.update = function (req, res) {
+    
+// }

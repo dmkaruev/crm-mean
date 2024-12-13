86864e850800001d4a1926666473d77517428605
Dmitry Karuev <dmkaruev@mail.ru>
2024-12-13 16:34:54 +0300

    Создание категорий (upd)

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const authRoutes = require('./routes/auth');
-// const analyticsRoutes = require('./routes/analytics');
+const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
-// const orderRoutes = require('./routes/order');
+const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
 const app = express();
@@ -31,9 +31,9 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
-// app.use('/api/analytics', analyticsRoutes);
+app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
-// app.use('/api/order', orderRoutes);
+app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
 
 module.exports = app;
--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,23 @@
-// module.exports.getAll = function (req, res) {
-    
-// }
+const Order = require('../models/Order');
+const errorHandler = require('../utils/errorHandler');
 
-// module.exports.create = function (req, res) {
-    
-// }
+module.exports.getAll = function (req, res) {
+
+}
+
+module.exports.create = async function (req, res) {
+    try {
+        const lastOrder = await Order
+            .findOne({ user: req.user.id })
+            .sort({ date: -1 });
+
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: maxOrder + 1
+        }).save();
+        res.status(201).json(order);
+    } catch (error) {
+        errorHandler(res, error);
+    }
+}

42f2b8c72bb3ac29420fe6232ae729cb95b29cc2
dmkaruev <dmkaruev@mail.ru>
2024-12-17 23:09:06 +0300

    Функционал заказов

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const authRoutes = require('./routes/auth');
-const analyticsRoutes = require('./routes/analytics');
+//const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
@@ -31,7 +31,7 @@ app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/analytics', analyticsRoutes);
+//app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
